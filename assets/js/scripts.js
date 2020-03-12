c29812521bd42ad174a3f15f1b6e00c526318c93
Michael-SN <michael.nascimento@codezone.com.br>
2020-03-12 15:28:42 -0300

    Validação do CPF criada e funcionando

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -44,33 +44,73 @@ function mascaraCNPJ () {
     }
 }
 
+window.addEventListener('keyup', e =>{    
+    if(e.keyCode === 13 )  validaCPF();   
+})
 const btnSubmit = document.querySelector('.btn-submit');
 btnSubmit.addEventListener('click', validaCPF);
 
-
-
+// 0 1 2 3 4 5 6 7 8 9 10 11 12 13
+// 8 2 0 . 8 6 3 . 8 0 0  -  5  4 - comparar os dois ultimos digitos e que se forem iguais o cpf é válido
 function validaCPF () {
-    const cpf = inputText.value;
-    const cpfLimpo = cpf.replace(/\D+/g,"").split(''); 
-    const cpfPartial = cpfLimpo.slice(0, -2);
-    let regress = cpfPartial.length + 1;
-    console.log(regress)
-    const digito = cpfPartial.reduce((ac, valor) => {
-        ac += (ac * Number(valor));
-        console.log(ac)
-        regress--;
-        return ac
-    }, 0);
-    console.log(digito);
-    // const result = calculaCPF(digito);
+    const cpf = inputText.value; // aqui entra o cpf completo 
+    const cpfLimpo = cpf.replace(/\D+/g,"").split(''); // aqui separa os digitos num array
+    const cpfPartial = cpfLimpo.slice(0, 9); // aqui separa os nove primeiros digitos 
+    const verificador = cpfLimpo.slice(-2);
+    const digito1 = calculaDigito(10, cpfPartial);        
+    
+    if(!((verificador[0]) == digito1)) {        
+        // ... alert danger
+        alerta('alert-danger', `${cpf} não é válido!!`);
+    } else {
+        cpfPartial.push(String(digito1));        
+        const digito2 = calculaDigito(11, cpfPartial);                
+
+        if(!((verificador[1] == digito2))) {
+            // alert danger
+            alerta('alert-danger', `${cpf} não é válido!!`);
+        }
+        else {
+            // alert sucessull
+            alerta('alert-success', `${cpf} é válido!!`);
+        }
+    }    
 }
 
-function calculaCPF(val) { 
-    return 11 - (val % 11);
+function calculaDigito(regress, arr ){ 
+    const soma = arr.reduce((acumalador, valorAtual) => {
+        const value = valorAtual * regress;
+        regress--;
+        return value + acumalador;
+    }, 0);
+
+    if((11 - (soma % 11)) > 9) return 0;
+    return (11 - (soma % 11));    
+}
+function criaAlerta() {
+    const alerts = document.querySelector('.alerts');    
+    const warning = document.createElement('div');   
+    alerts.appendChild(warning);
+    warning.classList.add('alert');
+    const p = document.createElement('p');    
+    warning.appendChild(p);
+}
+function alerta(className, msg){           
+    const alert = document.querySelector('.alert');
+    if(alert){        
+        const p = alert.children[0];
+        alert.classList.remove("alert-danger");
+        alert.classList.remove("alert-success");        
+        alert.classList.add(className);                
+        p.innerHTML = msg;
+    }
+    else {        
+        criaAlerta();
+        alerta(className, msg);
+    }
+
 }
 
 const btnClear = document.querySelector('.btn-clear');
 btnClear.addEventListener('click', clearInput);
 function clearInput() { inputText.value = ''; }
-
-
